71e1d255507cadf12537def4d09f0c96ccf9bab9
Honza Slesinger <slesinger@gmail.com>
2022-04-16 14:26:22 +0200

    fixing topic namespaces in launch

--- launch/bobik_launch.py
+++ launch/bobik_launch.py
@@ -2,17 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='bobik_driver',
-            namespace='bobik_driver',
-            executable='bobik_driver',
-            name='bobik_driver'
-        ),
-        Node(
-            package='xv_11_driver',
-            namespace='xv_11_driver',
-            executable='xv_11_driver',
-            name='lidar'
-        )
-    ])
+
+    start_bobik_driver = Node(
+        package='bobik_driver',
+        executable='bobik_driver',
+        name='bobik_driver',
+        output='screen',
+        arguments=['--honza', 'jenda']
+    )
+
+    start_xv_11_driver = Node(
+        package='xv_11_driver',
+        executable='xv_11_driver',
+        name='xv_11_driver',
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(start_bobik_driver)
+    ld.add_action(start_xv_11_driver)
+    return ld
